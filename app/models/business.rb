12f62fa890f7bd3cbf644ea6756544135300ff3a
Rachel Kat <rachelkat@gmail.com>
2020-06-06 14:36:29 -0400

    a website is unique to a business record and one business listed per listing

--- app/models/business.rb
+++ app/models/business.rb
@@ -3,4 +3,5 @@ class Business < ApplicationRecord
   has_many :business_listings
   has_many :lists, :through => :business_listings
   validates :website, :name, presence: true
+  validates :website, uniqueness: true
 end
