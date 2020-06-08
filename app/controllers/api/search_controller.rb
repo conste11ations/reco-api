5434f3d909f6bce307253eb3ca074368de4f89b7
Rachel Kat <rachelkat@gmail.com>
2020-06-08 18:31:10 -0400

    implement psql like

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,6 +1,12 @@
 class Api::SearchController < ApplicationController
   def index
-    @lists = List.that_sounds_like('MTL')
+    if params[:key] === 'list'
+    @lists = List.where('name ILIKE ?', '%' + params[:name] + '%')
+    elsif params[:key] === 'location'
+      @lists = List.where('location ILIKE ?', '%' + params[:name] + '%')
+    else
+      @lists = []
+    end
     render json: @lists
   end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,5 @@
 class List < ApplicationRecord
   include PgSearch::Model
-  pg_search_scope :that_sounds_like,
-                  against: :name
   has_many :recommendations
   has_many :businesses, :through => :recommendations
   validates :location, :name, presence: true
