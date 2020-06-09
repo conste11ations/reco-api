0a44cbae925275ce23a61f76f75a9975fee6aada
Rachel Kat <rachelkat@gmail.com>
2020-06-08 23:55:30 -0400

    added validations for search query

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,8 +1,8 @@
 class Api::SearchController < ApplicationController
   def index
-    if params[:key] === 'list'
+    if (params[:key] === 'list' && params[:term].present? && params[:term].length > 0)
     @lists = List.where('name ILIKE ?', '%' + params[:term] + '%')
-    elsif params[:key] === 'location'
+    elsif (params[:key] === 'location' && params[:term].present? && params[:term].length > 0)
       @lists = List.where('location ILIKE ?', '%' + params[:term] + '%')
     else
       @lists = []
