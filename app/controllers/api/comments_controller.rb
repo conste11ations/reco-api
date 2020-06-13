6c7270aa1039918ea9d4d865af101c31d11a0937
Ariane <ariane.fairlie@gmail.com>
2020-06-12 23:44:11 -0400

    bug: why is comment unpermitted param?

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -14,6 +14,6 @@ class Api::CommentsController < ApplicationController
   private
 
   def comment_params
-    params.permit(:because, :recommendation_id)
+    params.permit(:because, :comment, :recommendation_id)
   end
 end
