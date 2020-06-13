6bef0041dd813749b1de3ce143fee17708ef7f62
Rachel Kat <rachelkat@gmail.com>
2020-06-13 00:52:46 -0400

    add create function for business and code clean up for list params

--- app/controllers/api/businesses_controller.rb
+++ app/controllers/api/businesses_controller.rb
@@ -3,4 +3,17 @@ class Api::BusinessesController < ApplicationController
     @businesses = Business.all
     render json: @businesses
   end
+
+  def create
+    @business = Business.new(business_params)
+    if @business.save
+      render json: @comment
+    end
+  end
+
+  def business_params
+    params.require(:website).permit(:website, :name, :image)
+  end
+
 end
+
--- db/structure.sql
+++ db/structure.sql
@@ -10,6 +10,20 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+--
 -- Name: fuzzystrmatch; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -25,7 +39,7 @@ COMMENT ON EXTENSION fuzzystrmatch IS 'determine similarities and distance betwe
 
 SET default_tablespace = '';
 
-SET default_table_access_method = heap;
+SET default_with_oids = false;
 
 --
 -- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: -
